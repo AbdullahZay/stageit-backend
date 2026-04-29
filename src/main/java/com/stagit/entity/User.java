882bbbc3fdbe4bc236111dc02385b7787dfb0ef4
AbdullahZay <AbdullahZaylaee7@gmail.com>
2026-04-29 04:29:36 +0300

    feature: Add User entity complete - closes #1

---
+++ src/main/java/com/stagit/entity/User.java
@@ -0,0 +1,40 @@
+package com.stagit.entity;
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+
+
+@Data
+@Entity
+@Table(name ="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String bio;
+
+    @Column
+    private String profileImage;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Post> posts;
+}
