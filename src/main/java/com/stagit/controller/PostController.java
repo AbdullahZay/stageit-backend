d77ac18dfb707fbfd1298d0d24e51137fa46c62d
AbdullahZay <AbdullahZaylaee7@gmail.com>
2026-04-29 05:37:04 +0300

    feature: Add Post controller - closes #12

---
+++ src/main/java/com/stagit/controller/PostController.java
@@ -0,0 +1,44 @@
+package com.stagit.controller;
+
+
+import com.stagit.dto.PostRequest;
+import com.stagit.dto.PostResponse;
+import com.stagit.entity.Post;
+import com.stagit.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @PostMapping("")
+    public ResponseEntity<PostResponse> post(@RequestBody PostRequest request){
+        PostResponse post = postService.createPost(request);
+        return ResponseEntity.status(201).body(post);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<PostResponse>> getAllPosts() {
+        List<PostResponse> posts = postService.getAllPosts();
+        return ResponseEntity.ok(posts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponse> getPostById(@PathVariable Long id) {
+        PostResponse post = postService.getPostById(id);
+        return ResponseEntity.ok(post);
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<PostResponse>> getPostByCategory(@PathVariable String category) {
+        List<PostResponse> categoryPosts = postService.getPostByCategory(category);
+        return ResponseEntity.ok(categoryPosts);
+    }
+}
