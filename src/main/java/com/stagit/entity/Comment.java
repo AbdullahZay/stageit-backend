8c4638c02f4fae9a8e4b4329950e419de0f3690e
AbdullahZay <AbdullahZaylaee7@gmail.com>
2026-05-04 04:42:08 +0300

    feature: Add Comment entity and repository - closes #13

---
+++ src/main/java/com/stagit/entity/Comment.java
@@ -0,0 +1,33 @@
+package com.stagit.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name= "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "post_id")
+    private Post post;
+}
---
+++ src/main/java/com/stagit/repo/CommentRepository.java
@@ -0,0 +1,11 @@
+package com.stagit.repo;
+
+import com.stagit.entity.Comment;
+import com.stagit.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+}
