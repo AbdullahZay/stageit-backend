632828d99434de68b431746f2a2484573b25c29d
AbdullahZay <AbdullahZaylaee7@gmail.com>
2026-05-19 22:06:48 +0300

    chore: Add project config

---
+++ src/main/java/com/stagit/StagitApplication.java
@@ -0,0 +1,13 @@
+package com.stagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StagitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StagitApplication.class, args);
+	}
+
+}
